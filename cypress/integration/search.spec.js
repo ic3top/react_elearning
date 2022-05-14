ec9e4826b1fb486347edc710df01cddc84d58c54
ic3top <dvdvdv889@gmail.com>
2022-05-14 22:31:02 +0300

    Add cypress test for search

--- cypress/integration/search.spec.js
+++ cypress/integration/search.spec.js
@@ -1,22 +1,22 @@
-describe('Sample test', () => {
+describe('Search', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/');
   })
 
-    it('Should navigate to search by default', () => {
-        const valueToSearch = 'jurassic';
-        cy.url().should('include', '/search?genre=ALL')
-        cy.contains(/FIND YOUR MOVIE/i);
+  it('should display appropriate movies', () => {
+    const valueToSearch = 'jurassic';
+    cy.url().should('include', '/search?genre=ALL')
+    cy.contains(/FIND YOUR MOVIE/i);
 
-        cy.get('.search-input').type(valueToSearch);
+    cy.get('.search-input').type(valueToSearch);
 
-        cy.server();
-        cy.route('GET', '/movies');
+    cy.server();
+    cy.route('GET', '/movies');
 
-        cy.get('.button').click();
+    cy.get('.button').click();
 
-        cy.get('.movies__found > span').invoke('text').then(parseFloat).should('be.gt', 0);
-        cy.get('.movies').find('.movie-card__img').its('length').should('be.gt', 0);
-        cy.get('.movie-card__info > .movie-card__title').contains(new RegExp(valueToSearch, 'i'));
-    })
+    cy.get('.movies__found > span').invoke('text').then(parseFloat).should('be.gt', 0);
+    cy.get('.movies').find('.movie-card__img').its('length').should('be.gt', 0);
+    cy.get('.movie-card__info > .movie-card__title').contains(new RegExp(valueToSearch, 'i'));
+  });
 });
